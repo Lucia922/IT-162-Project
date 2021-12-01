8f48a3f50bb68e7f11fe8e459d8f530b31c5b043
Jinhee Byoun <jinheebyoun@Jinhees-MBP.domain>
2021-11-30 21:41:50 -0800

    Added lang="en" at header.php / Added flowchart.php linked google doc

---
+++ flowchart.php
@@ -0,0 +1,8 @@
+<?php
+include 'includes/header.php';
+?>
+
+<iframe src="https://docs.google.com/document/d/1kzyMexKXyhwDf07cREfMjuC2wC6chiAS00DrnSGdsjk/edit" width="1000" height="800">Sorry</iframe>
+</body>
+</html>
+
