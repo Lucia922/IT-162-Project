b7655be741b8c3b2fcecf03046b8f19c006b6311
Jinhee Byoun <jinheebyoun@Jinhees-MacBook-Pro.local>
2021-12-14 16:13:04 -0800

    Added fp/index.php

--- fp/index.php
+++ fp/index.php
@@ -2,8 +2,16 @@
 include 'includes/header.php';
 ?>
 
-<iframe src="https://rukusvelo.com/IT-162-Final-Project//" width="1000" height="800">Sorry</iframe>
+<section>
+   <h1>IT 162 Final Project</h1>
+   <p>
+   <a href="https://rukusvelo.com/IT-162-Final-Project/index.php">Visit rukusvelo.com!</a>
+   </p>
+</section>
 
 <?php
 include 'includes/footer.php';
 ?>
+
+
+
